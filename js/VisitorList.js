0636982ec48d456c2ed7be42f5e5e84873830dde
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2019-10-31 11:52:16 +0100

    Testar olika funktioner för rendering av objekt

--- js/VisitorList.js
+++ js/VisitorList.js
@@ -1,7 +1,6 @@
 class VisitorList extends Domer {
 
     visitors = [];
-    selectVisitorToRender = '';
     confirmCheckOut = '';
     idNumber;
 
@@ -16,54 +15,56 @@ class VisitorList extends Domer {
         console.log(this.visitors);
     }
 
-    businessVisitor() {
-        let businessVisitor = '';
-        for (let visitor of this.visitors) {
-            //if (this.visitors.department !== '') {
-            businessVisitor += `
-            <div>${visitor.dateTime}</div> 
-            <div>${visitor.fullName}</div>
-            <div>${visitor.idNumber}</div>
-            <div>${visitor.department}</div>   
-          `
-            //}
-
-        }
-        return businessVisitor;
-    }
-
-    maintenanceVisitor() {
-        let maintenanceVisitor = '';
-        for (let visitor of this.visitors) {
-            if (this.visitors.maintenance !== '') {
-                maintenanceVisitor += `
-            <div>${visitor.dateTime}</div> 
-            <div>${visitor.fullName}</div>
-            <div>${visitor.idNumber}</div>
-            <div>${visitor.workOrder}</div>   
-          `}
-            return maintenanceVisitor;
-        }
-    }
-
-    personalVisitor() {
-        let personalVisitor = '';
-        for (let visitor of this.visitors) {
-            if (this.visitors.personal !== '') {
-                personalVisitor += `
-            <div>${visitor.dateTime}</div> 
-            <div>${visitor.fullName}</div>
-            <div>${visitor.idNumber}</div>
-            <div>${visitor.personal}</div>   
-          `
+    /* Separata funktioner, test 1 */
+    /* 
+        businessVisitor() {
+            let businessVisitor = '';
+            for (let visitor of this.visitors) {
+                //if (this.visitors.department !== '') {
+                businessVisitor += `
+                <div>${visitor.dateTime}</div> 
+                <div>${visitor.fullName}</div>
+                <div>${visitor.idNumber}</div>
+                <div>${visitor.department}</div>   
+              `
+                //}
+    
             }
-            return personalVisitor;
+            return businessVisitor;
         }
-    }
+    
+        maintenanceVisitor() {
+            let maintenanceVisitor = '';
+            for (let visitor of this.visitors) {
+                if (this.visitors.maintenance !== '') {
+                    maintenanceVisitor += `
+                <div>${visitor.dateTime}</div> 
+                <div>${visitor.fullName}</div>
+                <div>${visitor.idNumber}</div>
+                <div>${visitor.workOrder}</div>   
+              `}
+                return maintenanceVisitor;
+            }
+        }
+    
+        personalVisitor() {
+            let personalVisitor = '';
+            for (let visitor of this.visitors) {
+                if (this.visitors.personal !== '') {
+                    personalVisitor += `
+                <div>${visitor.dateTime}</div> 
+                <div>${visitor.fullName}</div>
+                <div>${visitor.idNumber}</div>
+                <div>${visitor.personal}</div>   
+              `
+                }
+                return personalVisitor;
+            }
+        }
+     */
 
-    /* Renderar listan i div-element. */
-    checkVisitorToRender() {
-
+    /* Skapa variabler och testa mot villkor, test 2. */
+    /*
         if (this.visitors.department !== '') {
             return this.selectVisitorToRender = this.businessVisitor();
         }
@@ -74,40 +75,79 @@ class VisitorList extends Domer {
             return this.selectVisitorToRender = this.personalVisitor();
         }
 
-    }
+    }*/
 
-    /* let completeVisitorList = '';
-    for (let visitor of this.visitors) {
-        
-        if (visitor.department !== '') {
-            completeVisitorList += `
+    /* Skapa loop med villkor, test 3. */
+    /* checkVisitorToRender() {
+        let completeVisitorList = '';
+        for (let visitor of this.visitors) {
+            if (this.visitors.department !== '') {
+                completeVisitorList += `
         <div>${visitor.dateTime}</div> 
         <div>${visitor.fullName}</div>
         <div>${visitor.idNumber}</div>
         <div>${visitor.department}</div>   
       `}
-        else if (visitor.workOrder !== '') {
-            completeVisitorList += `
+            else if (this.visitors.workOrder !== '') {
+                completeVisitorList += `
         <div>${visitor.dateTime}</div> 
         <div>${visitor.fullName}</div>
         <div>${visitor.idNumber}</div>
         <div>${visitor.workOrder}</div>
       `
-        }
-        else {
-            completeVisitorList += `
+            }
+            else {
+                completeVisitorList += `
         <div>${visitor.dateTime}</div> 
         <div>${visitor.fullName}</div>
         <div>${visitor.idNumber}</div>
         <div>${visitor.personal}</div>
       `
+            }
+            return completeVisitorList;
+        }
+    } */
+
+    /* Loop i varje villkor, test 4.  */
+    
+    checkVisitorToRender() {
+        let completeVisitorList = '';
+
+        if (this.visitors.department !== '') {
+            for (let visitor of this.visitors) {
+                completeVisitorList += `
+        <div>${visitor.dateTime}</div> 
+        <div>${visitor.fullName}</div>
+        <div>${visitor.idNumber}</div>
+        <div>${visitor.department}</div>   
+      `}
+            return completeVisitorList;
+        }
+        else if (this.visitors.workOrder !== '') {
+            for (let visitor of this.visitors) {
+                completeVisitorList += `
+        <div>${visitor.dateTime}</div> 
+        <div>${visitor.fullName}</div>
+        <div>${visitor.idNumber}</div>
+        <div>${visitor.workOrder}</div>
+      `
+            }
+            return completeVisitorList;
+        }
+        else {
+            for (let visitor of this.visitors) {
+                completeVisitorList += `
+        <div>${visitor.dateTime}</div> 
+        <div>${visitor.fullName}</div>
+        <div>${visitor.idNumber}</div>
+        <div>${visitor.personal}</div>
+      `
+            }
+            return completeVisitorList;
         }
 
     }
-    return completeVisitorList; 
-    
-    }
-    */
+
 
 
     /* Utcheckningsfunktion */
@@ -135,7 +175,7 @@ class VisitorList extends Domer {
     render(html) {
         return html`
           <section id="render">
-          ${this.businessVisitor()}
+          ${this.checkVisitorToRender()}
           <p></p>
           </section>
         `
